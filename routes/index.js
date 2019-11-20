599051c3b6f334d10f96d81bbc1c23d4b43cedc3
Becka Catanzaro <becka@Beckas-MacBook-Pro.local>
2019-11-20 16:57:09 -0500

    used express-sessions to create basic working shopping cart skeleton

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 /* Dependencies */
 var express = require('express');
 var router = express.Router();
+var Cart = require("../models/cart.js");
 
 var Product = require("../models/product.js");
 
@@ -16,8 +17,27 @@ router.get("/", function(req, res, next) {
   });
 });
 
-router.get("/add-to-cart:id", function(req, res, next) {
+router.get("/add-to-cart/:id", function(req, res, next) {
+  var productId = req.params.id;
+  var cart = new Cart(req.session.cart ? req.session.cart : {});
 
+  Product.findById(productId, function(error, product) {
+    if (error) {
+      return res.redirect("/");
+    }
+    cart.add(product, product.id);
+    req.session.cart = cart;
+    console.log(req.session.cart);
+    res.redirect("/");
+  });
+});
+
+router.get("/shopping-cart", function(req, res, next) {
+  if (!req.session.cart) {
+    return res.render("shop/shopping-cart", {products:null});
+  };
+  var cart = new Cart(req.session.cart);
+  res.render("shop/shopping-cart", {products: cart.generateArray(), totalPrice: cart.totalPrice});
 });
 
 /* Export */
